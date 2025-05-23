55003e8f5e17648f1e35ec2f8fc8681a7189eb50
sickpickcode <tavyisthere@gmail.com>
2025-05-23 23:11:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.accordion-btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
+});
